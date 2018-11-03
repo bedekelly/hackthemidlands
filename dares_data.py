3b2545a8f85f55fb1591f0c44adcb3a0dd15e07d
Bede Kelly <bede.kelly@accenture.com>
2018-11-03 12:05:43 +0000

    Initial commit

---
+++ dares_data.py
@@ -0,0 +1,42 @@
+dares = """Take an embarrassing selfie and post it as your profile picture.
+Remove your socks with your teeth.
+Go next door with a measuring cup and ask for a cup of sugar.
+Let the group choose an item for you to brush your teeth with.
+Write your name on the floor with your tongue.
+Go outside and pick exactly 40 blades of grass with a pair of tweezers.
+Stick a Hot Cheeto in you nose and leave it there for 5 minutes.
+Eat a whole piece of paper.
+Lick a car tire.
+Open your front door and howl like a wolf for 30 seconds.
+I dare you to tie your hands to your ankles for the rest of the game.
+Put a bunch of honey on your nose and coat it with flour.
+Until the next round, talk super loud, like nobody can hear you.
+Call your crush.
+Take a shot of pickle juice.
+Talk to a pillow like it's your crush.
+Pretend you're a bird and eat cereal off the floor using only your mouth.
+Make out with your hand.
+Let someone else style your hair and keep it that way for the rest of the day.
+Use a brush like you're talking into a microphone each time you speak.
+Color one of your front teeth black. (Eyeliner works!)
+Pick your friend's nose.
+Lick the bottom of your shoe.
+Fake cry.
+Make repulsive sounds while eating and drinking.
+Cross your eyes when talking.
+Talk without closing your mouth.
+Act like an animal of the group's choosing.
+Get into a debate with a wall.
+Squirt your face with a water pistol continuously while talking.
+See how many grapes you can stuff in your mouth.
+Hiccup in between each word.
+Burp the alphabet.
+Draw on your face with permanent marker.
+Dump a bunch of LEGO on the floor and walk over it with your bare feet.
+Eat a spoonful of mustard.
+Jump into a dumpster.
+Spin around 10 times and try to walk straight.
+Eat a raw egg.
+Stand up and do jumping jacks until your next turn.
+Rub your armpits and then smell your fingers.
+Dig through the trash and name everything you find.""".split("\n")
---
+++ finished_main.py
@@ -0,0 +1,54 @@
+import random
+from pprint import pprint
+from redis import Redis
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+from dares_data import dares
+
+TABLE_NAME = "DARES"
+
+app = Flask(__name__)
+CORS(app)
+redis = Redis()
+
+
+def random_dare():
+    """Fetch a random dare from the database."""
+    all_dares = list(redis.hgetall(TABLE_NAME))
+    dare = random.choice(all_dares)
+    return dare.decode("utf-8")
+
+
+@app.route("/dare/all")
+def get_all_dares():
+    """Retrieve all the dares, with scores, from the database."""
+    items = redis.hgetall(TABLE_NAME).items()
+    dares = [ 
+        { 
+            "score": int(score.decode("utf-8")), 
+            "dare":  dare.decode("utf-8")
+        }
+        for (dare, score) in items 
+    ]
+    
+    dares = sorted(dares, key=lambda d: d["score"], reverse=True)
+    return jsonify(dares=dares)
+
+
+@app.route("/dare/vote", methods=["POST"])
+def vote_on_dare():
+    """Vote on a dare, and retrieve a random next dare."""
+    dare = request.json["dare"]
+    vote = request.json["vote"]
+    score_change = { "up": 1, "down": -1, "skip": 0 }[vote]
+    redis.hincrby(TABLE_NAME, dare, score_change)
+    return jsonify(dare=random_dare())
+
+
+# Create all the rows.
+for dare in dares:
+    redis.hset(TABLE_NAME, dare, 0)
+
+
+app.run(debug=True)
